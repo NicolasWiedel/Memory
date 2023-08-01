ef53d8e8f3fd436c284aef5e53e00019f700b679
NicolasWiedel <nicolaswiedel@googlemail.com>
2023-08-01 23:22:06 +0200

    2.2. Spiellogik Teil 2

--- Memory/Assets/Scripts/GameManager.cs
+++ Memory/Assets/Scripts/GameManager.cs
@@ -7,8 +7,17 @@ public class GameManager : MonoBehaviour
     public MemoryCard firstSelectedCard;
     public MemoryCard secondSelectedCard;
 
+    private bool canClick = true;
+
     public void CardClicked(MemoryCard card)
     {
+        if(canClick == false)
+        {
+            return;
+        }
+
+        card.transform.localEulerAngles = new Vector3(90, 0, 0);
+
         if(firstSelectedCard == null)
         {
             firstSelectedCard = card;
@@ -16,7 +25,31 @@ public class GameManager : MonoBehaviour
         else
         {
             secondSelectedCard = card;
+
+            canClick = false;
+
+            Invoke("CheckMatch", 1);
         }
        
+    }
+
+    private void CheckMatch()
+    {
+        if (firstSelectedCard.identifier == secondSelectedCard.identifier)
+        {
+            Destroy(firstSelectedCard.gameObject);
+            Destroy(secondSelectedCard.gameObject);
+        }
+
+        else
+        {
+            firstSelectedCard.transform.localEulerAngles = new Vector3(-90, 0, 0);
+            secondSelectedCard.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        }
+
+        firstSelectedCard = null;
+        secondSelectedCard = null;
+
+        canClick = true;
     }
 }
